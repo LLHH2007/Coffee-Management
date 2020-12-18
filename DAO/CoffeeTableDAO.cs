1aa4f0ccb1e2f6c527f6ed338364a2c30a8e1068
LH <74388049+huynhlongploc@users.noreply.github.com>
2020-12-18 11:25:12 +0700

    first commit

---
+++ DAO/CoffeeTableDAO.cs
@@ -0,0 +1,67 @@
+﻿
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeManagement
+{
+    class CoffeeTableDAO
+    {
+
+        public static int TableWidth = 80;
+        public static int TableHeight = 80;
+        private static CoffeeTableDAO instance;
+
+        public static CoffeeTableDAO Instance
+        {
+            get { if (instance == null) instance = new CoffeeTableDAO(); return CoffeeTableDAO.instance; }
+            private set { CoffeeTableDAO.instance = value; }
+        }
+
+        private CoffeeTableDAO() { }
+
+        public List<CoffeeTable> LoadTableList()
+        {
+            List<CoffeeTable> tableList = new List<CoffeeTable>();
+            string query = "select * from CoffeeTable";
+            DataTable data = ConnectDB.Instance.ExecuteQuery(query);
+            foreach(DataRow item in data.Rows)
+            {
+                CoffeeTable table = new CoffeeTable(item);
+                tableList.Add(table);
+            }
+
+            return tableList;
+        }
+
+        public List<CoffeeTable> LoadTableListOrderByTime() {
+            List<CoffeeTable> tableList = new List<CoffeeTable>();
+            string query = "SELECT CoffeeTable.* FROM CoffeeTable inner join Invoice on CoffeeTable.tableId = Invoice.tableId" +
+                " where invoiceStatus=1 order by invoiceId";
+            DataTable data = ConnectDB.Instance.ExecuteQuery(query);
+            foreach (DataRow item in data.Rows) {
+                CoffeeTable table = new CoffeeTable(item);
+                tableList.Add(table);
+            }
+
+            return tableList;
+        }
+
+        public bool UpdateTable(int tableID) {
+            bool result = false;
+            List<CoffeeTable> tableList = new List<CoffeeTable>();
+            string query = "Update CoffeeTable set tableStatus= 0 where tableId= @tableId";
+            MySqlParameter[] parameters = new MySqlParameter[1];
+            parameters[0] = new MySqlParameter("@tableId", MySqlDbType.Int32);
+            parameters[0].Value = tableID;
+            int data = ConnectDB.Instance.ExecuteNonQuery(query, parameters);
+            if (data != 0)
+                result = true;
+            return result;
+        }
+    }
+}
